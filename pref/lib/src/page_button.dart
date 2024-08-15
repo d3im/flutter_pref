3b6e792fc67e014fb5bff8c03a57cedf0cc535b2
deim <deim@test.cz>
2024-08-15 15:09:51 +0200

    own page as callable Type

--- pref/lib/src/page_button.dart
+++ pref/lib/src/page_button.dart
@@ -29,7 +29,7 @@ class PrefPageButton extends StatelessWidget {
   final Widget? leading;
   final Widget? trailing;
   final Widget? leadingAppBar;
-  final Widget? ownPage;
+  final Type? ownPage;
 
   @override
   Widget build(BuildContext context) {
